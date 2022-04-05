dfc6f93a1a46e8991b0d4f2f17dc2ebcecb99ec2
Kirill Suptelov <kirill.suptelov@gmail.com>
2022-04-05 22:45:47 +0700

    new file:   demo/.mvn/wrapper/maven-wrapper.jar 	new file:   demo/.mvn/wrapper/maven-wrapper.properties 	new file:   demo/mvnw 	new file:   demo/mvnw.cmd 	new file:   demo/pom.xml 	new file:   demo/src/main/java/com/github/thecoolersuptelov/demo/DemoApplication.java 	new file:   demo/src/main/java/com/github/thecoolersuptelov/demo/controllers/Cat.java 	new file:   demo/src/main/java/com/github/thecoolersuptelov/demo/controllers/MainController.java 	new file:   demo/src/main/java/com/github/thecoolersuptelov/demo/controllers/ProductsController.java 	new file:   demo/src/main/java/com/github/thecoolersuptelov/demo/entities/Product.java 	new file:   demo/src/main/java/com/github/thecoolersuptelov/demo/repositories/ProductRepository.java 	new file:   demo/src/main/java/com/github/thecoolersuptelov/demo/services/ProductsService.java 	new file:   demo/src/main/resources/application.properties 	new file:   demo/src/main/resources/db/migration/V1__init.sql 	new file:   demo/src/main/resources/templates/cat-form.html 	new file:   demo/src/main/resources/templates/hello.html 	new file:   demo/src/main/resources/templates/index.html 	new file:   demo/src/main/resources/templates/old/abc.html 	new file:   demo/src/main/resources/templates/old/cart-page.html 	new file:   demo/src/main/resources/templates/old/edit-product.html 	new file:   demo/src/main/resources/templates/old/index.html 	new file:   demo/src/main/resources/templates/old/info.html 	new file:   demo/src/main/resources/templates/old/international.html 	new file:   demo/src/main/resources/templates/old/login.html 	new file:   demo/src/main/resources/templates/old/navigation.html 	new file:   demo/src/main/resources/templates/old/order-before-purchase.html 	new file:   demo/src/main/resources/templates/old/order-filler.html 	new file:   demo/src/main/resources/templates/old/order-mail.html 	new file:   demo/src/main/resources/templates/old/product-page.html 	new file:   demo/src/main/resources/templates/old/registration-confirmation.html 	new file:   demo/src/main/resources/templates/old/registration-form.html 	new file:   demo/src/main/resources/templates/old/shop-page.html 	new file:   demo/src/main/resources/templates/product-page.html 	new file:   demo/src/main/resources/templates/products.html 	new file:   demo/src/main/resources/templates/simple-form.html 	new file:   demo/src/test/java/com/github/thecoolersuptelov/demo/DemoApplicationTests.java

---
+++ demo/src/main/java/com/github/thecoolersuptelov/demo/services/ProductsService.java
@@ -0,0 +1,31 @@
+package com.github.thecoolersuptelov.demo.services;
+
+
+import com.github.thecoolersuptelov.demo.entities.Product;
+import com.github.thecoolersuptelov.demo.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductsService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product getById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public void add(Product product) {
+        productRepository.save(product);
+    }
+}
